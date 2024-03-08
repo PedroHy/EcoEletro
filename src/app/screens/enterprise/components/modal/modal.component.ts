fbae3002d239464c45dc0b1b884cf5a51499b530
Elias Pinheiro <pinheirinho2004@gmail.com>
2024-03-08 13:00:46 -0300

    adress modal

---
+++ src/app/screens/enterprise/components/modal/modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+  @Output() exit = new EventEmitter<any>()
+
+  buttonHandler() {
+
+  }
+
+  exitClickHandler() {
+    this.exit.emit()
+  }
+}
--- src/app/screens/enterprise/enterprise.component.ts
+++ src/app/screens/enterprise/enterprise.component.ts
@@ -3,16 +3,17 @@ import { EnterpriseService } from '../../services/enterprise.service';
 import { Enterprise } from '../../interfaces/Enterprise';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-enterprise',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './enterprise.component.html',
   styleUrl: './enterprise.component.css'
 })
 export class EnterpriseComponent {
-
+  showModal: boolean = false;
   enterprise: Enterprise = {};
 
   constructor(private enterpriseService: EnterpriseService, private authService: AuthService, private router: Router) {
@@ -21,9 +22,16 @@ export class EnterpriseComponent {
     });
   }
 
-  logOut = ()=>{
+  logOut = () => {
     this.authService.signOut()
     this.router.navigateByUrl("/")
   }
 
+  buttonModalHandler() {
+    this.showModal = true
+  }
+
+  exitFormHandler() {
+    this.showModal = false
+  }
 }
